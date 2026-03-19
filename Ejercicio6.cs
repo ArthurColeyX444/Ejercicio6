b665922223dd61e3f2f9efee30561b35421c4930
Arthur <jarthurcoleyx@gmail.com>
2026-03-18 21:42:34 -0500

    s

---
+++ Ejercicio6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        
+        /// Calcula el promedio de una lista de números.
+        
+        /// <param name="numeros">Lista de números enteros</param>
+        /// <returns>Promedio de los números</returns>
+        public double CalcularPromedio(List<int> numeros)
+    {
+        if (numeros == null || numeros.Count == 0)
+        {
+            return 0;
+        }
+
+        double suma = 0;
+
+        foreach (int numero in numeros)
+        {
+            suma += numero;
+        }
+
+        double promedio = suma / numeros.Count;
+
+        return promedio;
+    }
+}
+}
